ccefa470be9cddcd2fe292169bd5eb84d34409c4
Ryan S <rsmall2007@my.fit.edu>
2021-03-26 20:17:42 -0700

    now more

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Sucka!'));
+app.get('/', (req, res) => res.send('Hello Sucka! Now there\'s more'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
